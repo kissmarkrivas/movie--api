0e3b35e4904efe42ee9c45ce47e204318cf849fc
kissmark <rivaskissmark@gmail.com>
2023-10-11 03:11:54 -0500

    add

---
+++ models/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+
+
+class Server{
+  constructor(){
+    this.app = express();
+    this.port = process.env.PORT;
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares(){
+    this.app.use(cors())
+    this.app.use( express.static('public'))
+  }
+
+  routes(){
+    this.app.use('/list_movies',require('../routes/listMovies'))
+    this.app.use('/characters',require('../routes/listCharacters'))
+    this.app.use('/characters/full',require('../routes/character'))
+  }
+
+  listen(){
+    this.app.listen(this.port, () => {
+      console.log(`Example app listening on this.port ${this.port}`)
+    })
+  }
+
+
+
+}
+
+module.exports = Server;
